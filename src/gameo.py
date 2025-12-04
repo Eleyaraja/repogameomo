620f1049288ae1f4bb411b9eb98a2cddb4927b75
Eleyaraja R <160092324+Eleyaraja@users.noreply.github.com>
2025-12-04 05:42:56 +0530

    Auto-save: src/gameo.py

--- src/gameo.py
+++ src/gameo.py
@@ -1,46 +1,30 @@
-# XOX Game in Python
+// TodoItem.tsx
+import React from 'react';
 
-# Initialize the game board
-board = [' ' for _ in range(9)]
+interface Todo {
+  id: string;
+  title: string;
+  done: boolean;
+}
 
-def print_board():
-    row1 = '| {} | {} | {} |'.format(board[0], board[1], board[2])
-    row2 = '| {} | {} | {} |'.format(board[3], board[4], board[5])
-    row3 = '| {} | {} | {} |'.format(board[6], board[7], board[8])
+interface TodoItemProps {
+  todo: Todo;
+  onToggleDone: (id: string) => void;
+  onRemove: (id: string) => void;
+}
 
-    print()
-    print(row1)
-    print(row2)
-    print(row3)
-    print()
+const TodoItem: React.FC<TodoItemProps> = ({ todo, onToggleDone, onRemove }) => {
+  return (
+    <li>
+      {todo.title}
+      <input
+        type="checkbox"
+        checked={todo.done}
+        onChange={() => onToggleDone(todo.id)}
+      />
+      <button onClick={() => onRemove(todo.id)}>Remove</button>
+    </li>
+  );
+};
 
-def check_win():
-    win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
-    for condition in win_conditions:
-        if board[condition[0]] == board[condition[1]] == board[condition[2]] != ' ':
-            return board[condition[0]]
-    if ' ' not in board:
-        return 'Tie'
-    return False
-
-def game():
-    current_player = 'X'
-    while True:
-        print_board()
-        move = input("Player {}, enter your move (1-9): ".format(current_player))
-        if board[int(move) - 1] == ' ':
-            board[int(move) - 1] = current_player
-            result = check_win()
-            if result:
-                print_board()
-                if result == 'Tie':
-                    print("It's a tie!")
-                else:
-                    print("Player {} wins! Congratulations!".format(result))
-                break
-            current_player = 'O' if current_player == 'X' else 'X'
-        else:
-            print("Invalid move, try again.")
-
-if __name__ == '__main__':
-    game()
+export default TodoItem;
