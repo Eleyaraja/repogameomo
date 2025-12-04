4e1b3952c878a9e0c150a7b9531f8525b3a161c8
Eleyaraja R <160092324+Eleyaraja@users.noreply.github.com>
2025-12-04 05:42:59 +0530

    Create gameo.py

--- src/gameo.py
+++ src/gameo.py
@@ -1,30 +1,3 @@
-// TodoItem.tsx
-import React from 'react';
+// gameo.py
+// Created with Axion Studio
 
-interface Todo {
-  id: string;
-  title: string;
-  done: boolean;
-}
-
-interface TodoItemProps {
-  todo: Todo;
-  onToggleDone: (id: string) => void;
-  onRemove: (id: string) => void;
-}
-
-const TodoItem: React.FC<TodoItemProps> = ({ todo, onToggleDone, onRemove }) => {
-  return (
-    <li>
-      {todo.title}
-      <input
-        type="checkbox"
-        checked={todo.done}
-        onChange={() => onToggleDone(todo.id)}
-      />
-      <button onClick={() => onRemove(todo.id)}>Remove</button>
-    </li>
-  );
-};
-
-export default TodoItem;
